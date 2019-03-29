8b8e40fb260685691b6c94a10578f370b0e42828
Prystauka Yauheni <34281826+Zheka1389@users.noreply.github.com>
2019-03-29 16:50:48 +0300

    1

---
+++ NET.S.2019.Pristavko.04/Converter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2019.Pristavko._04
+{
+    public static class Converter
+    {
+        /// <summary>
+        /// static extention method DoubleHelper for Double
+        /// </summary>
+        /// <param name="doubleNumber"></param>
+        /// <returns>double to string</returns>
+        public static string DoubleToBinaryString(this double doubleNumber)
+        {
+            StringBuilder sb = new StringBuilder();
+            byte[] doubleByte = BitConverter.GetBytes(doubleNumber);
+            foreach (byte b in doubleByte)
+                for (int i = 0; i < 8; i++)
+                {
+                    sb.Insert(0, ((b >> i) & 1) == 1 ? "1" : "0");
+                }
+            return sb.ToString();
+        }
+    }
+}
